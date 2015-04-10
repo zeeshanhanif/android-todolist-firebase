6ef7c7bcbedf674e45a111e75d562b94f9c70c28
Zeeshan Hanif <zeeshanhanif@gmail.com>
2015-04-10 23:20:46 +0500

    some refactor code

--- FirebaseToDoList/app/src/main/java/firebasetodolist/todolist/firebase/com/LoginActivity.java
+++ FirebaseToDoList/app/src/main/java/firebasetodolist/todolist/firebase/com/LoginActivity.java
@@ -1,7 +1,6 @@
 package firebasetodolist.todolist.firebase.com;
 
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,15 +8,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 
 
-public class ToDoListActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_to_do_list);
+        setContentView(R.layout.activity_login);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -59,7 +57,7 @@ public class ToDoListActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_to_do_list, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_login, container, false);
             return rootView;
         }
     }
